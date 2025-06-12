a31a456e9cf67041af64a330c790ecc98d377c69
andres noriega <anoriega182@gmail.com>
2025-06-12 14:41:25 +0000

    Actualice la pagina, me dirigio directamente al inicio, pero esta tardan

---
+++ next.config.js
@@ -0,0 +1,32 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/inicio',
+        permanent: true, // Usamos true para una redirección permanente (SEO amigable)
+      },
+    ]
+  },
+};
+
+export default nextConfig;
