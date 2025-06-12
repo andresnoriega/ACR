6b4614339edd24abf5050cc172bae0e018c8794c
andres noriega <anoriega182@gmail.com>
2025-06-12 14:43:44 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/inicio',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
