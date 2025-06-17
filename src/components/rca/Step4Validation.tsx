f066929846f6e016f0a2455fbb704c3765e8e77c
andres noriega <anoriega182@gmail.com>
2025-06-17 15:48:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/rca/Step4Validation.tsx
+++ src/components/rca/Step4Validation.tsx
@@ -1,5 +1,6 @@
 
 'use client';
+import * as React from "react"; // Added this line
 import type { FC } from 'react';
 import { useMemo, useState } from 'react';
 import type { PlannedAction, Validation, FullUserProfile, Evidence } from '@/types/rca';
