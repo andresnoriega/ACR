ca1241b5bb1d7062b09373ed0356098efadf9431
andres noriega <anoriega182@gmail.com>
2025-06-17 00:44:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -54,17 +54,15 @@ TableFooter.displayName = "TableFooter"
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, children, ...props }, ref) => ( // Explicitly destructure children
+>(({ className, children, ...props }, ref) => (
   <tr
     ref={ref}
     className={cn(
       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
       className
     )}
-    {...props} // Spread the rest of the props
-  >
-    {children} {/* Render children explicitly */}
-  </tr>
+    {...props}
+  >{children}</tr>
 ))
 TableRow.displayName = "TableRow"
 
