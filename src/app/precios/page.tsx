b26c461a9a001df063d1a1a7f92d9e529dd39a15
andres noriega <anoriega182@gmail.com>
2025-06-27 23:01:25 +0000

    Acabo de publicar nuevamente la aplicación, alguna forma de distrasar, e

--- src/app/precios/page.tsx
+++ src/app/precios/page.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-// This page has been disabled as per the user's request.
+import { useEffect } from 'react';
+import { redirect } from 'next/navigation';
+
+// This page has been disabled and now redirects to the home page.
 export default function PreciosPage() {
+  useEffect(() => {
+    redirect('/inicio');
+  }, []);
+
   return null;
 }
