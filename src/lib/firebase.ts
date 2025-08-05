25ec35ee860d9231d7e4fef5b273157359bd8da9
andres noriega <anoriega182@gmail.com>
2025-08-05 02:55:49 +0000

    Revierte este cambio

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore, memoryLocalCache } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // =================================================================================
@@ -28,7 +28,9 @@ if (!getApps().length) {
 }
 
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = initializeFirestore(app, {
+  localCache: memoryLocalCache(),
+});
 const storage = getStorage(app);
 
 export { app, auth, db, storage, firebaseConfig };
