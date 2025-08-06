4d827255303ad5b6d6311b45b4b7f67be944c050
andres noriega <anoriega182@gmail.com>
2025-08-06 12:51:56 +0000

    WTF. Eliminaste la pagina de inicio!!!

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,12 +9,13 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANyy8c6Mtz5eEsEISIFBfc0Il5nm2dj8Y",
-  authDomain: "assetflow-7qwu2.firebaseapp.com",
-  projectId: "assetflow-7qwu2",
-  storageBucket: "assetflow-7qwu2.appspot.com",
-  messagingSenderId: "176933832283",
-  appId: "1:176933832283:web:3c08259cb2ec5a3799617e"
+  apiKey: "AIzaSyBpRAXR8mTcBTXwuXV5VaXdqCP6yx85MUE",
+  authDomain: "almacenador-cloud.firebaseapp.com",
+  projectId: "almacenador-cloud",
+  storageBucket: "almacenador-cloud.firebasestorage.app",
+  messagingSenderId: "790911154631",
+  appId: "1:790911154631:web:91e2d71d8ccfbf058301e2",
+  measurementId: "G-R2NQTYM2GX"
 };
 
 // Initialize Firebase
